fe4e1c9422ff5cee7b7583543bc113f0ee032d6c
Alexander Kahl <userrrrr@server.fake>
2013-06-09 13:36:20 +0200

    new TransferObject for REST

---
+++ src/main/java/com/kahl/twitterwall/entity/TweetStatesTransferObject.java
@@ -0,0 +1,13 @@
+package com.kahl.twitterwall.entity;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class TweetStatesTransferObject {
+
+    public List<String> tweetIds;
+
+}
